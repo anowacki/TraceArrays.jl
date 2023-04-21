4f4ee5281066b6d8ba2666a2d65c3d1f007a627e
Andy Nowacki <a.nowacki@leeds.ac.uk>
2023-04-21 09:20:54 +0100

    Implement `Seis.normalise[!]` (and `normalize[!]`)
    Implement channel-by-channel normalisation for `AbstractTraceArray`s,
    but also add an extra `all` keyword argument which, when `true`, will
    normalise all channels to retain their relative amplitudes.

--- src/operations.jl
+++ src/operations.jl
@@ -45,3 +45,48 @@ function _cut_time_indices(t::AbstractTraceArray, b, e; warn=true, allowempty=fa
     ie = Seis.nsamples(t) - round(Int, (Seis.endtime(t) - e)/t.delta)
     ib, ie
 end
+
+"""
+    normalise!(t::AbstractTraceArray, val=1; all=false) -> t
+
+Normalise all channels in `t` to have maximum absolute value `val`.
+
+By default, this is done on a channel-by-channel basis.
+To normalise all channels simultaneously, preserving the inter-channel
+relative amplitudes, use `all=true`.  In this case, the single
+maximum absolute amplitude across all the data will be normalised
+to `val` instead.
+"""
+function Seis.normalise!(t::AbstractTraceArray, val=1; all=false)
+    if all
+        _normalise_all!(Seis.trace(t), val)
+    else
+        _normalise_columns!(Seis.trace(t), val)
+    end
+    t
+end
+
+"""
+    _normalise_columns!(data::AbstractMatrix, val)
+
+Normalise all columns in `data` to have the maximum absolute value
+`val`.
+"""
+function _normalise_columns!(data::AbstractMatrix, val)
+    @inbounds for icol in axes(data, 2)
+        maxval = maximum(abs, @view(data[:,icol]))
+        data[:,icol] .= @view(data[:,icol]) .* (val/maxval)
+    end
+end
+
+"""
+    _normalise_all!(data, val)
+
+Normalise all columns in `data` such that the maximum absolute value
+in the whole matrix is `val`.
+"""
+function _normalise_all!(data::AbstractMatrix, val)
+    maxval = maximum(abs, data)
+    data .= data .* val/maxval
+    nothing
+end
